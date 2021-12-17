dc1b1caddea6b2bb2334766c1e966ac6a1f40f16
Kylian <kylian.paulin@epitech.eu>
2021-12-17 14:58:03 +0100

    pour ahmed

--- rasa-assistant/actions/actions.py
+++ rasa-assistant/actions/actions.py
@@ -7,21 +7,28 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+class ActionTime(Action):
+
+    def name(self) -> Text:
+        return "action_intent"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        events = tracker.current_state()['events']
+        user_events = []
+        for e in events:
+            if e['event'] == 'user':
+                user_events.append(e)
+        
+        print(user_events[-1]['metadata'])
+
+        dispatcher.utter_message(text=f"{tracker.latest_message}")
+
+        return []
